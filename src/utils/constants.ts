a712f8ad15082a389794e5f33a69357a58cb70be
Ivan Shukhov <45999338+ivanshukhov@users.noreply.github.com>
2024-11-29 16:02:44 +0100

    update address

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 import { BigInt } from '@graphprotocol/graph-ts'
 export const META_MANAGER_ADDRESS = '0xBCa208eF255e74Ac586d2A7B659d44F50a140974'
-export const PHYSICA_TOKEN_FACTORY_ADDRESS = '0xf3C553747EF0fBAaA5c3154746336F16E8EE4273'
+export const PHYSICA_TOKEN_FACTORY_ADDRESS = '0xa2725D76926b46C9860a68b70D959b2c3C535EB0'
 
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
